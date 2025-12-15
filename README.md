0968603cf8a61f4afad3785378dcacca8d30ee67
Akhil0605 <akhileshkovelakuntla.com>
2025-12-15 11:17:44 +0530

    exception and logging is completed

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 
 # NetWorkSecurity
+
+## this is my nework security project
+
+### Progress So Far
+
+- Set up the project structure and initialized the repository.
+- Researched key concepts in network security.
+- Identified potential threats and vulnerabilities to address.
+- Drafted an initial plan for implementing security measures.
+- Installed necessary libraries and tools for the project.
--- setup.py
+++ setup.py
@@ -1,1 +1,34 @@
 
+"""
+This script is a setup file typically used in Python projects to define metadata and configuration for the package. 
+It is used to specify details such as the package name, version, author, dependencies, and other relevant information. 
+The setup file is essential for packaging and distributing the project, making it easier for others to install and use.
+
+# Why setup file is used:
+# The setup file is used to automate the installation process, manage dependencies, and provide metadata about the project. 
+# It ensures that the package can be easily shared and installed in a consistent manner.
+"""
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requiremnts()->List(str):
+    ## this function will return the list of requirements
+    try:
+        with open("requirements.txt","r") as f:
+            lines=f.readlines()
+            for line in lines:
+                requirement=line.strip()
+                # ignore empty lines and comments
+                if requirement and requirement!="-e .":
+                    requirement_lst.append(requirement) 
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Akhilesh",
+    author_email="akhileshkovelakuntla@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requiremnts(),
+)
