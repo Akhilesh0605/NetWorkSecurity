b0700e63d0f56d06d52eb2da05b3281f44223ee8
Akhil0605 <akhileshkovelakuntla.com>
2025-12-15 21:19:59 +0530

    mongo db connected

--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 LOG_FILE=f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log'
 
-log_path=os.path.join(os.getgwd(),"logs",LOG_FILE)
+log_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(log_path,exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
