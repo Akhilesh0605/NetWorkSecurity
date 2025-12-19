92c6103aee3e7124f2bb6cdc007260037f406869
Akhil0605 <akhileshkovelakuntla.com>
2025-12-19 18:33:14 +0530

    training completed

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.components.data_transformation import DataTransformation
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig,DataValidationConfig,DataTransformationConfig
-from networksecurity.components.data_transformation import DataTransformation
+
+from networksecurity.entity.config_entity import (
+    DataIngestionConfig, 
+    TrainingPipelineConfig,
+    DataValidationConfig,
+    DataTransformationConfig,
+    ModelTrainerConfig
+    )
+
 
 import sys
 
@@ -25,7 +36,11 @@ if __name__=="__main__":
         data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         logging.info("Data Transformation is completed")
-        print(data_transformation_artifact)
+        logging.info("Model Training Started")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config,data_transformation_artifact)
+        model_trainer_artifact=model_trainer.intiate_model_trainer()
+        logging.info("model traing is completed")
         
     except Exception as e:
         raise NetworkSecurityException(e,sys)
---
+++ networksecurity/utils/ml_utils/metric/classification_metric.py
@@ -0,0 +1,18 @@
+from networksecurity.entity.artifact_entity import ClassificationMetricArtifact
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from sklearn.metrics import f1_score, precision_score, recall_score
+
+def get_classification_score(y_true, y_pred) -> ClassificationMetricArtifact:
+    try:
+        f1 = f1_score(y_true, y_pred)
+        precision = precision_score(y_true, y_pred)
+        recall = recall_score(y_true, y_pred)
+        classification_metric_artifact = ClassificationMetricArtifact(
+            f1_score=f1,
+            precision_score=precision,
+            recall_score=recall
+        )
+        return classification_metric_artifact 
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
