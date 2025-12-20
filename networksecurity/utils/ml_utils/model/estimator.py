fe3db70e3f6d4d2420be53245f152b7d45f2730a
Akhil0605 <akhileshkovelakuntla.com>
2025-12-20 22:06:42 +0530

    pipeline is completed

--- networksecurity/utils/ml_utils/model/estimator.py
+++ networksecurity/utils/ml_utils/model/estimator.py
@@ -1,3 +1,5 @@
+import sys
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 
@@ -12,8 +14,8 @@ class NetworkModel:
             raise NetworkSecurityException(e,sys)
     def predict(self,x):
         try:
-            self.x_transform=self.preprocessor.transform(x)
+            x_transform=self.preprocessor.transform(x)
             y_hat=self.model.predict(x_transform)
             return y_hat
         except Exception as e:
-            raise NetworkSecurityException
+            raise NetworkSecurityException(e,sys)
